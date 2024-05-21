0e4e2c8c4bfd30b2a1a53aa7dce3a11b9f5d457a
chaiheung <chaiheung@naver.com>
2024-05-21 12:02:32 +0900

    post /api/board/add 메소드 작성

---
+++ src/main/java/com/caprj240521backend/controller/board/BoardController.java
@@ -0,0 +1,16 @@
+package com.caprj240521backend.controller.board;
+
+import com.caprj240521backend.domain.board.Board;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/board")
+public class BoardController {
+    @PostMapping("add")
+    public void add(@RequestBody Board board) {
+        System.out.println("board = " + board);
+    }
+}
