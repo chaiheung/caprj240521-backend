79105541004dfe47b867461cfe8f6393ccdcccd0
chaiheung <chaiheung@naver.com>
2024-05-21 14:31:19 +0900

    게시물 목록 보기

--- src/main/java/com/caprj240521backend/mapper/board/BoardMapper.java
+++ src/main/java/com/caprj240521backend/mapper/board/BoardMapper.java
@@ -3,6 +3,9 @@ package com.caprj240521backend.mapper.board;
 import com.caprj240521backend.domain.board.Board;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface BoardMapper {
@@ -12,4 +15,11 @@ public interface BoardMapper {
             VALUES (#{title}, #{content}, #{writer})
             """)
     public int insert(Board board);
+
+    @Select("""
+            SELECT id, title, writer 
+            FROM board
+            ORDER BY id DESC   
+            """)
+    List<Board> selectAll();
 }
