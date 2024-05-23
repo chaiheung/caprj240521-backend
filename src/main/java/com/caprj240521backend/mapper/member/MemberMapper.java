83ad80b5d4aff45f1d546031d6a105f30a7dc3cf
chaiheung <chaiheung@naver.com>
2024-05-23 10:16:57 +0900

    회원 정보 변경

--- src/main/java/com/caprj240521backend/mapper/member/MemberMapper.java
+++ src/main/java/com/caprj240521backend/mapper/member/MemberMapper.java
@@ -1,10 +1,7 @@
 package com.caprj240521backend.mapper.member;
 
 import com.caprj240521backend.domain.member.Member;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -54,4 +51,11 @@ public interface MemberMapper {
             WHERE id = #{id}
             """)
     int deleteById(Integer id);
+
+    @Update("""
+            UPDATE member
+            SET password = #{password}, nick_name = #{nickName}
+            WHERE id = #{id}
+            """)
+    int update(Member member);
 }
