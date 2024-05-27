d4a723dcff70df030f50bde35f0aee6cee2267cd
chaiheung <chaiheung@naver.com>
2024-05-27 15:11:17 +0900

    조회용 업데이트

--- sql/project.sql
+++ sql/project.sql
@@ -78,3 +78,32 @@ SELECT title, content, member_id
 FROM board;
 SELECT COUNT(*)
 FROM board;
+
+SELECT *
+FROM member;
+UPDATE member
+SET nick_name = 'abcd'
+WHERE id = 5;
+UPDATE member
+SET nick_name = 'efgh'
+WHERE id = 6;
+
+UPDATE board
+SET member_id = 5
+WHERE id % 2 = 0;
+UPDATE board
+SET member_id = 6
+WHERE id % 2 = 1;
+
+UPDATE board
+SET title   = 'abc def',
+    content = 'ghi jkl'
+WHERE id % 3 = 0;
+UPDATE board
+SET title   = 'mno pqr',
+    content = 'stu vwx'
+WHERE id % 3 = 1;
+UPDATE board
+SET title   = 'yz1 234',
+    content = '567 890'
+WHERE id % 3 = 2;
