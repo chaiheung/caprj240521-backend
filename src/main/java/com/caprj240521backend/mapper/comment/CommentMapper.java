c5a0095649fe017c7148cee94126efcecbc1514a
chaiheung <chaiheung@naver.com>
2024-05-30 12:16:52 +0900

    댓글 목록 응답

--- src/main/java/com/caprj240521backend/mapper/comment/CommentMapper.java
+++ src/main/java/com/caprj240521backend/mapper/comment/CommentMapper.java
@@ -3,6 +3,9 @@ package com.caprj240521backend.mapper.comment;
 import com.caprj240521backend.domain.comment.Comment;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface CommentMapper {
@@ -12,5 +15,13 @@ public interface CommentMapper {
             (board_id, member_id, comment)
             VALUES (#{boardId}, #{memberId}, #{comment})
             """)
-    int insert(Comment comment);
+    public int insert(Comment comment);
+
+    @Select("""
+            SELECT *
+            FROM comment
+            WHERE board_id = #{boardId}
+            ORDER BY id
+            """)
+    List<Comment> selectAllByBoardId(Integer boardId);
 }
