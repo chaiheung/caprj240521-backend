4855bc32b32160d858b884becfb51baa5e57fd26
chaiheung <chaiheung@naver.com>
2024-05-31 12:00:39 +0900

    댓글 작성 시간 보기 수정

--- src/main/java/com/caprj240521backend/domain/comment/Comment.java
+++ src/main/java/com/caprj240521backend/domain/comment/Comment.java
@@ -3,6 +3,7 @@ package com.caprj240521backend.domain.comment;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class Comment {
@@ -12,4 +13,17 @@ public class Comment {
     private String comment;
     private LocalDateTime inserted;
     private String nickName;
+    LocalDateTime beforeOneDay = LocalDateTime.now().minusDays(1);
+
+    public String getInserted() {
+        LocalDateTime beforeOneDay = LocalDateTime.now().minusDays(1);
+
+        if (inserted.isBefore(beforeOneDay)) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            return inserted.format(formatter).toString();
+        } else {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            return inserted.format(formatter).toString();
+        }
+    }
 }
